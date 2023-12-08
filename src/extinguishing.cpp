4102fa0edbfdd2a697f2ac9559da01b227afb9d2
YuraBeznos <git@you-ra.info>
2023-12-08 09:22:05 +0300

    feat: more modules code

--- src/extinguishing.cpp
+++ src/extinguishing.cpp
@@ -7,18 +7,50 @@
 
 using namespace std;
 
+string me = "extinguishing";
+bool action = false;
+
+Broker* broker;
+
+void
+proceed_action()
+{
+  action = true;
+  cout << "action (fire/extinguishing) has been started" << endl;
+}
+
+void
+stop_action()
+{
+  action = false;
+  cout << "action (fire/extinguishing) has been stopped" << endl;
+}
+
 void
 Broker::on_packet(packet pkt)
 {
+  if (pkt.method == "action") {
+    packet out_pkt = { me.c_str(), "eaic", "action" };
+    broker->to_monitor(out_pkt);
+  } else if (pkt.method == "start_action") {
+    proceed_action();
+
+  } else if (pkt.method == "stop_action") {
+    stop_action();
+    packet out_pkt = { me.c_str(), "eaic", "stop_action" };
+    broker->to_monitor(out_pkt);
+  }
 }
 
 int
 main(int argc, char** argv)
 {
-  cout << "start: " << argv[0] << endl;
-  while (1) {
-    cout << "working: " << argv[0] << endl;
-    sleep(10);
-  }
+  cout << "start: " << me << endl;
+
+  mosqpp::lib_init();
+  broker = new Broker(me);
+  broker->subscribe(NULL, me.c_str());
+  broker->loop_forever();
+  mosqpp::lib_cleanup();
   return 0;
 }
--- src/movement.cpp
+++ src/movement.cpp
@@ -1,4 +1,5 @@
 #include "broker.h"
+#include "utils.h"
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
@@ -8,18 +9,39 @@
 
 using namespace std;
 
+string me = "movement";
+
+Broker* broker;
+
+bool
+to_position(string task)
+{
+  cout << "going to position of: " << task;
+  sleep(5); // TODO: logic of movement to coordinates
+  return true;
+}
+
 void
 Broker::on_packet(packet pkt)
 {
+  if (pkt.method == "move") {
+    string task = pkt.val1;
+    if (to_position(task)) {
+      packet out_pkt = { me.c_str(), pkt.from, "done", task };
+      broker->to_monitor(out_pkt);
+    }
+  }
 }
 
 int
 main(int argc, char** argv)
 {
-  cout << "start: " << argv[0] << endl;
-  while (1) {
-    cout << "working: " << argv[0] << endl;
-    sleep(10);
-  }
+  cout << "start: " << me << endl;
+
+  mosqpp::lib_init();
+  broker = new Broker(me);
+  broker->subscribe(NULL, me.c_str());
+  broker->loop_forever();
+  mosqpp::lib_cleanup();
   return 0;
 }
